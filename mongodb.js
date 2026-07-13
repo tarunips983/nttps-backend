ca5a3ca45553c4ae22dcdb04d59f67ae84478973
tarunips983 <102345359+tarunips983@users.noreply.github.com>
2026-07-13 17:36:51 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -2,31 +2,29 @@ import { MongoClient, GridFSBucket } from "mongodb";
 
 const uri = process.env.MONGO_URI;
 
-if (!uri) {
-  throw new Error("Missing MONGO_URI environment variable");
-}
-
 const client = new MongoClient(uri);
 
 let db;
 let bucket;
 
 export async function connectMongo() {
-  if (db && bucket) {
-    return { db, bucket };
-  }
+    if (db) return;
 
-  await client.connect();
+    await client.connect();
 
-  db = client.db("tmcam");
+    db = client.db("tmcam");
 
-  bucket = new GridFSBucket(db, {
-    bucketName: "files"
-  });
+    bucket = new GridFSBucket(db,{
+        bucketName:"files"
+    });
 
-  console.log("✅ MongoDB Connected");
-
-  return { db, bucket };
+    console.log("✅ MongoDB Connected");
 }
 
-export { db, bucket };
+export function getDB(){
+    return db;
+}
+
+export function getBucket(){
+    return bucket;
+}
